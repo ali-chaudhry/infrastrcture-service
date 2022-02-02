77b8705589742485b2582c8cbcdad3056d893cd7
Ali Anwar <ali.anwar33@gmail.com>
2022-02-02 15:49:06 +0500

    data types

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go conference"
-	const tickets = 50
+	var conferenceName string = "Go conference"
+	const tickets int = 50
 	fmt.Printf("Welcome to %v booking application. \n", conferenceName)
 	fmt.Printf("Only %v tickets left. \n", tickets)
 
