c9edab22dd83d490052dda335b7c646b1df226e6
Ali Anwar <ali.anwar33@gmail.com>
2022-02-02 15:43:48 +0500

    datatypes

--- main.go
+++ main.go
@@ -6,5 +6,15 @@ func main() {
 	var conferenceName = "Go conference"
 	const tickets = 50
 	fmt.Printf("Welcome to %v booking application. \n", conferenceName)
-	fmt.Printf("Only %v tickets left.", tickets)
+	fmt.Printf("Only %v tickets left. \n", tickets)
+
+	var userName string
+	var userTickets int
+	userName = "Ali"
+	userTickets = 10
+
+	fmt.Printf("User %v has bought %v tickets. ",userName, userTickets)
+
+
+
 }
