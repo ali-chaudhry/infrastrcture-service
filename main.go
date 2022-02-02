4d0356090afbf7fafa45e7a3d0d00009d4a738cb
Ali Anwar <ali.anwar33@gmail.com>
2022-02-02 15:32:18 +0500

    printting formatted string

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	var conferenceName = "Go conference"
 	const tickets = 50
-	fmt.Println("Welcome to ", conferenceName, "booking application")
-	fmt.Println("Only ", tickets, " tickets left")
+	fmt.Printf("Welcome to %v booking application. \n", conferenceName)
+	fmt.Printf("Only %v tickets left.", tickets)
 }
