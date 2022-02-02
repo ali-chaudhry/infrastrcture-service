8a159e71decbc890a14d4d2d9a07077a8650cc70
Ali Anwar <ali.anwar33@gmail.com>
2022-02-02 15:25:33 +0500

    constants

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go conference"
+	const tickets = 50
 	fmt.Println("Welcome to ", conferenceName, "booking application")
-
+	fmt.Println("Only ", tickets, " tickets left")
 }
