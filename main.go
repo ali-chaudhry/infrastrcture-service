39d77871b4917bbe9d2975176414a3608d8ddc70
Ali Anwar <ali.anwar33@gmail.com>
2022-02-03 10:08:07 +0500

    first loop added

--- main.go
+++ main.go
@@ -1,28 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	conferenceName := "Go conference"
 	const tickets int = 50
-	// uinit cannot have a negative value 
-	var remainingTickets uint = 50;
-
+	// uinit cannot have a negative value
+	var remainingTickets uint = 50
+	bookings := []string{}
 	fmt.Printf("Welcome to %v booking application. Total tickets are %v . \n", conferenceName, tickets)
 
 	var userName string
-	var userTickets int 
+	var userTickets int
 	var purchaseRatio float64
 
 	userName = "Ali"
 	userTickets = 10
-	remainingTickets -= uint(userTickets);
+	remainingTickets -= uint(userTickets)
 	purchaseRatio = (float64(remainingTickets) / float64(tickets)) * 100
 
-	fmt.Printf("User %v has bought %v tickets. ",userName, userTickets)
+	fmt.Printf("User %v has bought %v tickets. ", userName, userTickets)
 	fmt.Printf("Only %v tickets left. \n", remainingTickets)
 	fmt.Printf("Sold %v pecent of the the tcikets \n", purchaseRatio)
 
+	// for loop
+	// infinite lopp
 
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email:")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter your tickets:")
+		fmt.Scan(&userTickets)
+
+		remainingTickets -= userTickets
+		bookings = append(bookings, firstName+"  "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive confirmation email at %v .\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+		fmt.Printf("These are all our bookings: %v \n", bookings)
+
+	}
 
 }
